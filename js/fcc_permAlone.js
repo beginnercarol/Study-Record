841429acc564a639a236ce9ae55348cdf3cc6246
carolp <carol@CaroldeMBP.lan>
2017-09-16 16:54:25 +0800

    20 push 数组的时机不对

--- js/fcc_permAlone.js
+++ js/fcc_permAlone.js
@@ -1,6 +1,7 @@
 (function(){
 	var begin=0;
 	var perm = [];//存放所有全排列
+	var re = /(.)\1+/g;//正则表达式 寻找重复的模式
 
 	function exchange(arr,index1,index2){
 		var temp = arr[index1];
@@ -12,13 +13,26 @@
 		var origArr = arr.slice();
 		if(begin===arr.length-1){
 			//结束递归
+			perm.push(origArr.join(""));
 		}else{
-			for(let i=0;i<origArr.length;i++){//何时将全排列的字符串 push 进 perm 数组?
+			for(let i=0;i+begin<origArr.length;i++){//何时将全排列的字符串 push 进 perm 数组?
 				exchange(origArr,begin,begin+i);
+				perm.push(origArr.join(""));
 				permutation(origArr,begin+1);
 				exchange(origArr,begin,begin+i);
 			}
 		}
 	}
+
+	function permAlone(str){
+		var arr = str.split("");
+		permutation(arr,begin);
+		perm = perm.filter(function(val){
+			return !(val.search(re)>=0);//只需要判断是否存在用 search 就够了
+		});
+		return perm;
+	}
+
+	console.log(permAlone('aab'));
 
 })();
