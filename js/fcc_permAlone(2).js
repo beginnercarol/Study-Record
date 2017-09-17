4d3a34440c698e395cac7c5bd7b93c39e1ee93c7
carolp <carol@CaroldeMBP.lan>
2017-09-17 10:09:36 +0800

    no message

---
+++ js/fcc_permAlone(2).js
@@ -0,0 +1,21 @@
+//The question is when to push the recomibined string into final array...
+//The former didn't work
+(function(){
+	var begin=0;
+	var perm = [];//存放所有全排列
+	var re = /(.)\1+/g;//正则表达式 寻找重复的模式
+
+	function exchange(arr,index1,index2){
+		var temp = arr[index1];
+		arr[index1] = arr[index2];
+		arr[index2] = temp;
+	}
+
+	function permutation(arr,begin){
+
+	}
+
+
+
+
+})();
