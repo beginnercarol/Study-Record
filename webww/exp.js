604d18e08bde265f4b7ae389cdf9a3632c06c542
beginnercarol <42361487@qq.com>
2017-10-26 10:22:16 +0800

    webv2.0

--- webww/exp.js
+++ webww/exp.js
@@ -1,9 +1,11 @@
 window.onload = function(){
     //轮播图片
   (function(){
+    var slogans=["数学建模专业团队","985、211院校本科硕士博士","各类数模比赛预定ing"];
     var flag = 0;//用于记录当前的 slides 是哪一副
     var imgsDiv = document.querySelectorAll(".carousel-inner .item");
     console.log(imgsDiv);
+    var slogan = document.getElementById("slogan");
     var controls = document.querySelectorAll(".carousel-indicator li");
     for(var i=0;i<controls.length;i++){
         controls[i].onclick = (function(index){
@@ -14,17 +16,17 @@ window.onload = function(){
                     var classList = imgsDiv[j].className;
                     //console.log(classList);
                     //classList = Array.from(classList);
+
                     imgsDiv[j].className = toggleClass(classList,'active');
                 }
                 imgsDiv[index].className += ' active';
+                slogan.innerText = slogans[index];
                 flag=index%3;
             }
         })(i);
     }
 
-    function changeSlides(){
-
-        
+    function changeSlides(){ 
         //return function(){
             console.log("flag: "+flag);
             for(var j=0;j<imgsDiv.length;j++){
@@ -32,6 +34,7 @@ window.onload = function(){
                 //console.log(classList);
                 imgsDiv[j].className = toggleClass(classList,'active');
             }
+            slogan.innerText = slogans[flag];
             imgsDiv[flag].className += ' active';
             flag = (flag+1)%3;
             console.log("flag: "+flag);
@@ -55,6 +58,31 @@ window.onload = function(){
 
   })();
 
+
+(function(){
+    var imgModal = document.querySelectorAll(".worksShow .head-pic img");
+    var imgSrc = ['./temp/data-01.png','./temp/data-02.png','./temp/data-03.png','./temp/data-04.png'];
+    var myModal = document.querySelector(".myModal");
+    var myModalImg = document.querySelector(".myModal img");
+    var myModalClose = document.querySelector('.myModal>span');
+    var height = document.querySelector(".worksShow .head-pic").offsetHeight;
+    var modalBg = document.getElementById("modal-wrap");
+    for(let i=0;i<imgModal.length;i++){
+        imgModal[i].onclick  = (function(index){
+            return (function(event){
+                var imgSrc = event.target.src;
+                var rect = event.target.getBoundingClientRect();
+                myModal.style.setProperty('top',(height*Math.floor(index/2)+height*0.05)+'px');
+                myModal.style.setProperty('display','block');
+                myModalImg.setAttribute('src',imgSrc);
+                //modalBg.style.setProperty('opacity',0.6);
+            });
+        })(i);
+    }
+    myModalClose.onclick = function(){
+        myModal.style.setProperty('display','none');
+    }
+})();
 
 
 
