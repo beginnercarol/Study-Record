66b7149551fbeaf7247cb92c4c2f0ac23100ae0d
beginnercarol <42361487@qq.com>
2017-10-27 12:09:56 +0800

    carousel component

--- react-my-website/src/views/components/carousel.js
+++ react-my-website/src/views/components/carousel.js
@@ -7,6 +7,7 @@ class Carousel extends React.Component {
 	constructor(props){
 		super(props);
 		this.ctrNodes = [];
+		this.indiNodes = [];
 		//this.imgList = [];
 		this.length = this.props.imgs.length;
 		this.state={
@@ -15,14 +16,16 @@ class Carousel extends React.Component {
 			flag:0
 		};
 		this.handleClick = this.handleClick.bind(this);
-		this.handleClickLeft = this.handleClickLeft(this);
-		this.handleClickRight = this.handleClickRight(this);
+		this.handleClickLeft = this.handleClickLeft.bind(this);
+		this.handleClickRight = this.handleClickRight.bind(this);
 	}
 	componentWillMount(){
 		for(let i=0;i<this.length;i++){
 				this.ctrNodes.push(<li id={i} key={i} onClick={this.handleClick}></li>);
 				//this.imgList.push(<img src={this.props.imgs[i].src} alt={this.props.imgs[i].alt} />);
 		}
+		this.indiNodes.push(<span key="0" className="carousel-left" onClick={this.handleClickLeft}>&lt;</span>);
+		this.indiNodes.push(<span key="1" className="carousel-right" onClick={this.handleClickRight}>&gt;</span>);
 	}
 	handleClick(event){
 		//console.log(this.props.imgs[event.target.id].src);
@@ -33,24 +36,32 @@ class Carousel extends React.Component {
 		});
 	}
 	handleClickLeft(){
-		console.log("here:"+(this.state.flag-1)%this.length);
+		var flag=this.state.flag;
+		var index = (flag==0)?(this.length-1):(flag-1);
+		console.log("left:"+index);
 		this.setState({
-			src:this.props.imgs[(this.state.flag-1)%this.length].src,
-			alt:this.props.imgs[(this.state.flag-1)%this.length].alt,
+			src:this.props.imgs[index].src,
+			alt:this.props.imgs[index].alt,
+			flag:index
 		});
+		//console.log("here:"+((this.state.flag==0)?(this.length-1):(this.state.flag-1));
+
 	}
 	handleClickRight(){
+		var flag=this.state.flag;
+		var index = (flag==this.length)?(0):(flag+1);
+		console.log("right:"+index);
 		this.setState({
-			src:this.props.imgs[(this.state.flag+1)%this.length].src,
-			alt:this.props.imgs[(this.state.flag+1)%this.length].alt,
+			src:this.props.imgs[index].src,
+			alt:this.props.imgs[index].alt,
+			flag:index
 		});
 	}
 	render(){
 		//console.log(this.length);
 		return(
-			<div className="myCarousel">
-				<span className="carousel-left" onClick={this.handleClickLeft}>&lt;</span>
-				<span className="carousel-right" onClick={this.handleClickRight}>&gt;</span>
+			<div className="myCarousel clearfix">
+				{this.indiNodes}
 				<ul className="carousel-indicator">{this.ctrNodes}</ul>
 				<img className="carousel-item" src={this.state.src} alt={this.state.alt} />
 			</div>
