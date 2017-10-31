666b56ec4d1555540152fdff83001327fc6c38ef
beginnercarol <42361487@qq.com>
2017-10-31 12:33:49 +0800

    some improvement (adaptive)

---
+++ web/index.js
@@ -0,0 +1,110 @@
+window.onload = function(){
+    //轮播图片
+  (function(){
+    var slogans=["数学建模专业团队","985、211院校本科硕士博士","各类数模比赛预定"];
+    var flag = 0;//用于记录当前的 slides 是哪一副
+    var imgsDiv = document.querySelectorAll(".carousel-inner .item");
+    console.log(imgsDiv);
+    var slogan = document.getElementById("slogan");
+    var controls = document.querySelectorAll(".carousel-indicator li");
+    for(var i=0;i<controls.length;i++){
+        controls[i].onclick = (function(index){
+            //console.log(imgsDiv[index].getAttribute('class'));
+            return function(){
+                for(var j=0;j<imgsDiv.length;j++){
+                    var classList = imgsDiv[j].className;
+                    //console.log(classList);
+                    //classList = Array.from(classList);
+
+                    imgsDiv[j].className = toggleClass(classList,'active');
+                }
+                imgsDiv[index].className += ' active';
+                slogan.innerText = slogans[index];
+                flag=index%3;
+            }
+        })(i);
+    }
+
+    function changeSlides(){ 
+        //return function(){
+            console.log("flag: "+flag);
+            for(var j=0;j<imgsDiv.length;j++){
+                var classList = imgsDiv[j].className;
+                //console.log(classList);
+                imgsDiv[j].className = toggleClass(classList,'active');
+            }
+            slogan.innerText = slogans[flag];
+            imgsDiv[flag].className += ' active';
+            flag = (flag+1)%3;
+            console.log("flag: "+flag);
+            //console.log(flag);
+        //}    
+    }
+
+    function toggleClass(classList,item){
+        var arr = classList.split(" ");
+        var index = arr.indexOf(item);
+        //console.log(index);
+        if(index>=0){
+            //console.log((arr.slice(0,index)).concat(arr.slice(index+1)));
+            return ((arr.slice(0,index).concat(arr.slice(index+1))).join(" "));
+        }else{
+            return arr.join(" ");
+        }
+    }
+    var intervalId = setInterval(changeSlides,5000);
+
+    var imgSmBarcode = document.querySelector('img.smBarcode');
+    var imgXlBarcode = document.querySelector('span.bigBarcode');
+    imgSmBarcode.onclick = function(){
+        console.log(imgXlBarcode.className);
+        let xlClass = imgXlBarcode.className;
+        let newClass = 'bigBarcodeActive';
+        xlClass = xlClass.split(" ");
+        let index  = xlClass.indexOf(newClass);
+        if(index>=0){
+            xlClass = (xlClass.slice(0,index).concat(xlClass.slice(index+1))).join(" ");
+        }else{
+            xlClass = xlClass.concat(newClass).join(" ");
+            console.log(xlClass);
+        }
+        imgXlBarcode.className = xlClass;
+        console.log(imgXlBarcode.className);
+    }
+  })();
+
+
+(function(){
+    var imgModal = document.querySelectorAll(".worksShow .head-pic img");
+    var imgSrc = ['./temp/data-01.png','./temp/data-02.png','./temp/data-03.png','./temp/data-04.png'];
+    var myModal = document.querySelector(".myModal");
+    var myModalImg = document.querySelector(".myModal img");
+    var myModalClose = document.querySelector('.myModal>span');
+    //var height = document.querySelector(".worksShow .head-pic").offsetHeight;
+    var modalBg = document.getElementById("modal-wrap");
+    for(let i=0;i<imgModal.length;i++){
+        imgModal[i].onclick  = (function(index){
+            return (function(event){
+                var imgSrc = event.target.src;
+                //var rect = event.target.getBoundingClientRect();
+                //myModal.style.setProperty('top',(height*Math.floor(index/2)+height*0.05)+'px');
+                myModal.style.setProperty('display','block');
+                myModalImg.setAttribute('src',imgSrc);
+                //modalBg.style.setProperty('opacity',0.6);
+            });
+        })(i);
+    }
+    myModalClose.onclick = function(){
+        myModal.style.setProperty('display','none');
+    }
+})();
+
+(function(){
+    
+})();
+
+
+
+    
+}
+
