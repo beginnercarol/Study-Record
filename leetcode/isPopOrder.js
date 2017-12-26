b8c9f4abedb53a22ce71700303c69e297fe00931
beginnercarol <42361487@qq.com>
2017-12-26 16:09:47 +0800

    判断出栈序列 修改后的版本

--- leetcode/isPopOrder.js
+++ leetcode/isPopOrder.js
@@ -1,17 +1,35 @@
 //判断 popV 序列 是否是 入栈序列 pushV 对应的出栈序列
 (function(){
 	function IsPopOrder(pushV, popV){
-		var i=0;
+		var i=0,j=0;
 		var stack = [];
-		while(i<popV.length && pushV.length>0){
-			if(popV[i]!=(stack.length==0?stack[0]:stack[stack.length-1])){
-				stack.push(pushV.shift());
-			}else{
+		while(i<popV.length && j<popV.length){
+			if(pushV.length==0){
+				i++;
+			}
+			if(pushV.length>0)stack.push(pushV.shift());
+			if(popV[i]==stack[stack.length-1])){
 				stack.pop();
 				i++;
 			}
+
 		}
 		return stack.length == 0;
 	}
-	console.log(IsPopOrder([1,2,3,4,5],[4,5,2,3,1]));
+	// console.log(IsPopOrder([1,2,3,4,5],[4,5,2,3,1]));
+})();
+//修改版本
+(function(){
+	function IsPopOrder(pushV,popV){
+		var j=0;
+		var stack = [];
+		for(var i=0;i<pushV.length.i++){
+			stack.push(pushV[i]);
+			while(stack.length && (stack[stack.length-1]==popV[j]) && j<popV.length){
+				stack.pop();
+				j++;
+			}
+		}
+		return stack.length==0;
+	}
 })();
